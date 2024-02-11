dd97a131dc245742f27b70525190e078d4012822
ador05 <122834286+ador05@users.noreply.github.com>
2024-02-11 04:38:06 -0400

    modification 2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function validateGame() {
     const name01 = document.getElementById("input-nombre1").value;
 
@@ -10,6 +9,11 @@ function validateGame() {
     
     const tamano = document.getElementById("input-tamano").value;
 
+    localStorage.setItem("nombre01", name01)
+    localStorage.setItem("nombre02", name02)
+    localStorage.setItem("nombre03", name03)
+    localStorage.setItem("nombre04", name04)
+
     let verify1 = true;
     
     if(name01  == "" || name02 == "" || name03== "" || name04==""){
@@ -20,7 +24,7 @@ function validateGame() {
     
     
     if (verify1 && (tamano == "5" || tamano == "4"|| tamano == "3" )){
-        console.log("si llega a verificar")
+        
         window.scrollBy({ top: 600, behavior: 'smooth' });
         }   
         matriz01 = crearmatriz();
@@ -28,9 +32,32 @@ function validateGame() {
         matriz03 = crearmatriz();
         matriz04 = crearmatriz();
 
-        
+        localStorage.setItem("matriz1", matriz01);
+        localStorage.setItem("matriz2", matriz02);
+        localStorage.setItem("matriz2", matriz02);
+        localStorage.setItem("matriz2", matriz02);
+
+
     }
     
+function sacarbola()    {
+    let values = new Set();
+    let counter;
+    while (true)
+    {
+        counter = getRandomInt(1,50);
+        if (!values.has(counter)){
+            let bola = counter;
+            values.add(counter);
+            break;
+        }
+    }
+    return bola;
+}
+
+function guardar(){
+    console.log(localStorage.getItem("nombre01"));
+}
 
 function openindex2(){
     window.location.href = "./index2.html";
